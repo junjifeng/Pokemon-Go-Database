d44ccf80417780220c0901dd188d9c941ac6bbe9
Tom Ross <tom@umpox.com>
2016-07-15 00:09:53 +0100

    Add custom data types

--- app/pages/item-details/item-details.ts
+++ app/pages/item-details/item-details.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 
-
 @Component({
   templateUrl: 'build/pages/item-details/item-details.html'
 })
@@ -10,6 +9,6 @@ export class ItemDetailsPage {
 
   constructor(private nav: NavController, navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    this.selectedItem = navParams.get('pokemon');
   }
 }
